0f3dd243a369b2ba5796817fef0fdd94a56d7e9d
teambits <teambitsandy@gmail.com>
2024-06-21 16:07:47 +0300

    navbarsection

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-import React from 'react' 
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
 
-const App = () 
+const App = () => {
+     return(
+      <>
+      <Navbar/>
+      </>
+     );
+};
+
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 export default {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+      "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"##ffc001",
+        secondary:"#ff9c01",
+      }
+       },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:"1rem",
+        sm:"3rem",
+      },
+    },
   },
   plugins: [],
 }
